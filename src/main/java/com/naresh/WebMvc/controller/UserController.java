c458c1ae59765d94d551e710d0234b3f8877dd37
trapThisFly <i.am.naresh.online@gmail.com>
2026-09-16 11:30:28 +0530

    User Registration

--- src/main/java/com/naresh/WebMvc/controller/UserController.java
+++ src/main/java/com/naresh/WebMvc/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.naresh.WebMvc.controller;
 
+import com.naresh.WebMvc.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
+
 @Controller
-@RequestMapping("/")
+@RequestMapping("/user")
 public class UserController {
 
   @RequestMapping(value = "/home", method = RequestMethod.GET)
@@ -16,5 +21,19 @@ public class UserController {
   @RequestMapping(value = "/about", method = RequestMethod.GET)
   public String profile() {
     return "about";
+  }
+
+
+  @RequestMapping(value = "/register",method = RequestMethod.GET)
+  public String registerPage() {
+    return "user-form";
+  }
+
+  @RequestMapping(value = "/register",method = RequestMethod.POST)
+  public String register(@ModelAttribute("user")User user, BindingResult result) {
+    if(result.hasErrors()){
+      return "user-form";
+    }
+    return "success";
   }
 }
